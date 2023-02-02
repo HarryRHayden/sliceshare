67a6007fafbacf8a51c6b5b3487cc726842d9967
Harry Hayden <h_man1992@hotmail.com>
2023-02-02 22:59:42 +0000

    style and link up navbar to initial pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,43 @@
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
+import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo2.png";
-
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <Navbar fixed="top" expand="md">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
             <img src={logo} alt="logo" height="45" />
-        </Navbar.Brand>
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
-                <i className="fas fa-home"></i>Home
-            </Nav.Link>
-            <Nav.Link>
-            <i className="fas fa-sign-in-alt"></i>Sign In
-            </Nav.Link>
-            <Nav.Link>
-                <i className="fas fa-user-plus"></i>Sign Up
-            </Nav.Link>
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fas fa-home"></i>Home
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </NavLink>
+            <NavLink
+              to="/signup"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
+              <i className="fas fa-user-plus"></i>Sign up
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
