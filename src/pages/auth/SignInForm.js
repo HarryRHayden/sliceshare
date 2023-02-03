62368e24809c191aeb77c693c5e035a0bd871ba8
Harry Hayden <h_man1992@hotmail.com>
2023-02-03 14:28:40 +0000

    add sign in form with css and error catching

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
@@ -13,37 +13,114 @@ import { Link } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import axios from "axios";
 
 function SignInForm() {
-//   Add your component logic here
+    const [signInData, setSignInData] = useState({
+        username: "",
+        password: "",
+    });
+    const { username, password } = signInData;
 
-  return (
-    <Row className={styles.Row}>
-      <Col className="my-auto p-0 p-md-2" md={6}>
-        <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>Sign In</h1>
-          {/* Add your form here */}
+    const [errors, setErrors] = useState({})
 
-        </Container>
-        <Container className={`mt-3 ${appStyles.Content}`}>
-          <Link className={styles.Link} to="/signup">
-            Don't have an account? <span>Sign up now!</span>
-          </Link>
-        </Container>
-      </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
-      >
-        <Image
-          className={`${appStyles.FillerImage}`}
-          src={"https://res.cloudinary.com/djzr6htcy/image/upload/v1675428132/pizza-sign-in_iirzhz.avif"}
-          width={400}
-          height={400}
-        />
-      </Col>
-    </Row>
-  );
+    const handleChange = (event) => {
+        setSignInData({
+            ...signInData,
+            [event.target.name]: event.target.value
+        });
+    }
+
+    const history = useHistory();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post("/dj-rest-auth/login/", signInData);
+            history.push("/")
+        } catch(err) {
+            setErrors(err.response?.data);
+        }
+    }
+
+    return (
+        <Row className={styles.Row}>
+        <Col className="my-auto p-0 p-md-2" md={6}>
+            <Container className={`${appStyles.Content} p-4 `}>
+            <h1 className={styles.Header}>Sign In</h1>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="username">
+                    <Form.Label className="d-none">
+                        Username
+                    </Form.Label>
+                    <Form.Control
+                        className={styles.Input}
+                        type="text" 
+                        placeholder="username"
+                        name="username"
+                        value={username}
+                        onChange={handleChange}
+                    />
+
+                </Form.Group>
+                {errors.username?.map((message, idx) => (
+                    <Alert key={idx} variant="info">
+                        {message}
+                    </Alert>
+                ))}
+
+                <Form.Group controlId="password">
+                <Form.Label className="d-none">Password</Form.Label>
+                    <Form.Control
+                        className={styles.Input}
+                        type="password" 
+                        placeholder="Password"
+                        name="password"
+                        value={password}
+                        onChange={handleChange}
+                    />
+                </Form.Group>
+                {errors.password?.map((message, idx) => (
+                    <Alert key={idx} variant="info">
+                        {message}
+                    </Alert>
+                ))}
+
+                <Button
+                    className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+                    type="submit"
+                >
+                    Sign In
+                </Button>
+                {errors.non_field_errors?.map((message, idx) => (
+                    <Alert key={idx} variant="warning" className="mt-3">
+                        {message}
+                    </Alert>
+                ))}
+            </Form>
+            </Container>
+            <Container className={`mt-3 ${appStyles.Content}`}>
+            <Link className={styles.Link} to="/signup">
+                Don't have an account? <span>Sign up now!</span>
+            </Link>
+            </Container>
+        </Col>
+        <Col
+            md={6}
+            className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
+        >
+            <Image
+            className={`${appStyles.FillerImage}`}
+            src={
+                "https://res.cloudinary.com/djzr6htcy/image/upload/v1675428132/pizza-sign-in_iirzhz.avif"
+            }
+            width={400}
+            height={400}
+            />
+        </Col>
+        </Row>
+    );
 }
 
 export default SignInForm;
