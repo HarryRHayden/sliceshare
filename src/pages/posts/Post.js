b5f1d34aafd0e8d0c84734f15c2428501795ef8f
Harry Hayden <h_man1992@hotmail.com>
2023-02-04 16:28:52 +0000

    add post page setup and css

---
+++ src/pages/posts/Post.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "../../styles/PostPage.module.css";
+
+const Post = (props) => {
+  return (
+    <div>Post</div>
+  )
+}
+
+export default Post
--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -12,7 +12,9 @@ import Upload from "../../assets/upload_image.png";
 import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
-import { Image } from "react-bootstrap";
+import { Alert, Image } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function PostCreateForm() {
 
@@ -24,6 +26,9 @@ function PostCreateForm() {
     image: "",
   });
   const { title, content, image } = postData;
+
+  const imageInput = useRef(null);
+  const history = useHistory();
 
   const handleChange = (event) => {
     setPostData({
@@ -42,6 +47,24 @@ function PostCreateForm() {
     }
   }
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+
+    formData.append('title', title)
+    formData.append('content', content)
+    formData.append('image', imageInput.current.files[0])
+
+    try {
+      const {data} = await axiosReq.post('/posts/', formData);
+      history.push(`/posts/${data.id}`)
+    } catch(err) {
+      if (err.response?.status !== 401){
+        setErrors(err.response?.data)
+      }
+    }
+  }
+
   const textFields = (
     <div className="text-center">
       <Form.Group>
@@ -56,6 +79,12 @@ function PostCreateForm() {
           className={styles.Input}
         />
       </Form.Group>
+      {errors?.title?.map((message, idx) => (
+        <Alert variant="info" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
       <Form.Group>
         <Form.Label>Content</Form.Label>
         <Form.Control
@@ -67,10 +96,15 @@ function PostCreateForm() {
           className={styles.TextArea}
         />
       </Form.Group>
+      {errors?.content?.map((message, idx) => (
+        <Alert variant="info" key={idx}>
+          {message}
+        </Alert>
+      ))}
     
       <Button
         className={`${btnStyles.Button} ${btnStyles.Bright}`}
-        onClick={() => {}}
+        onClick={() => history.goBack()}
       >
         Cancel
       </Button>
@@ -81,7 +115,7 @@ function PostCreateForm() {
   );
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Row>
         <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
           <Container
@@ -107,13 +141,26 @@ function PostCreateForm() {
                   className="d-flex justify-content-center"
                   htmlFor="image-upload"
                 >
-                  <Asset src={Upload} message="Click or tap to share your pizza greatness" />
+                  <Asset
+                    src={Upload}
+                    message="Click or tap to share your pizza greatness"
+                  />
                 </Form.Label>
               )}
-                
-                <Form.File id="image-upload" accept="image/*" onChange={handleChangeImage}/>
 
+              <Form.File
+                id="image-upload"
+                accept="image/*"
+                onChange={handleChangeImage}
+                ref={imageInput}
+              />
             </Form.Group>
+            {errors?.image?.map((message, idx) => (
+              <Alert variant="info" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
             <div className="d-md-none">{textFields}</div>
           </Container>
         </Col>
