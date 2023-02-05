bab7bd46c1f6fc9c23658e95e5a10a4d0bfd1462
Harry Hayden <h_man1992@hotmail.com>
2023-02-05 18:59:18 +0000

    add routing for feed, home and liked post pages

--- src/App.js
+++ src/App.js
@@ -7,14 +7,44 @@ import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
 import PostCreateForm from './pages/posts/PostCreateForm';
 import PostPage from './pages/posts/PostPage';
+import { useCurrentUser } from './contexts/CurrentUserContext';
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home Page</h1>} />
+          <Route 
+            exact 
+            path="/" 
+            render={() => (
+              <PostPage message="No results found. Search for something else!" />
+            )} 
+          />
+          <Route 
+            exact 
+            path="/feed" 
+            render={() => (
+              <PostPage 
+                message="No results found. Search for something else or follow more users!"
+                filter={`owner__followed__owner__profile=${profile_id}&`}
+              />
+            )} 
+          />
+          <Route 
+            exact 
+            path="/" 
+            render={() => (
+              <PostPage 
+                message="No results found. Search for something else or like some posts!" 
+                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+              />
+            )} 
+          />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
