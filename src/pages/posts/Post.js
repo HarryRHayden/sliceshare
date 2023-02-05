858c1427f8c1046f5f76a3510237c79569258077
Harry Hayden <h_man1992@hotmail.com>
2023-02-05 17:02:13 +0000

    add design for Post Page

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,9 +1,44 @@
 import React from 'react'
 import styles from "../../styles/PostPage.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Card, Media } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Avatar from "../../components/Avatar";
 
 const Post = (props) => {
+  const {
+    id,
+    owner,
+    profile_id,
+    profile_image,
+    comments_count,
+    likes_count,
+    like_id,
+    title,
+    content,
+    image,
+    uploaded_at
+  } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
   return (
-    <div>Post</div>
+    <Card className={styles.Post}>
+      <Card.Body>
+        <Media className="align-items-center justify-content-between">
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} height={60}/>
+            {owner}
+          </Link>
+          <div className='d-flex align-items-center'>
+            <span>
+              {updated_at}
+            </span>
+          </div>
+        </Media>
+      </Card.Body>
+    </Card>
   )
 }
 
