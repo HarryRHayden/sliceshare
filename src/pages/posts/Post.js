6a381553fa4c9de1a063e601ea814020b8de3413
Harry Hayden <h_man1992@hotmail.com>
2023-02-05 18:25:28 +0000

    add like/unlike functionality. fix missing ===

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import Avatar from "../../components/Avatar";
 import appStyles from "../../App.module.css";
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { axiosRes } from '../../api/axiosDefaults';
 
 const Post = (props) => {
   const {
@@ -22,10 +23,43 @@ const Post = (props) => {
     image,
     updated_at,
     postPage,
+    setPosts,
   } = props;
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+
+  const handleLike = async () => {
+    try {
+      const { data } = await axiosRes.post("/likes/", { post: id});
+      setPosts((prevPosts) => ({
+        ...prevPosts,
+        results: prevPosts.results.map((post) => {
+          return post.id === id
+          ? {...post, likes_count: post.likes_count + 1, like_id: data.id }
+          : post;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUnlike = async () => {
+    try {
+      await axiosRes.delete(`/likes/${like_id}`);
+      setPosts((prevPosts) => ({
+        ...prevPosts,
+        results: prevPosts.results.map((post) => {
+          return post.id === id
+          ? {...post, likes_count: post.likes_count - 1, like_id: null }
+          : post;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <Card className={styles.Post}>
@@ -55,11 +89,11 @@ const Post = (props) => {
               <i className="far fa-thumbs-up" />
             </OverlayTrigger>
           ): like_id ? (
-            <span onClick={() => {}}>
+            <span onClick={handleUnlike}>
               <i className={`fas fa-thumbs-up ${styles.ThumbUp}`} />
             </span>
           ) : currentUser ? (
-            <span onClick={() => {}}>
+            <span onClick={handleLike}>
               <i className={`far fa-thumbs-up ${styles.ThumbOutline}`} />
             </span>
           ) : (
