022a9f48bdc9c8b87669968d2f6ae9008ff36451
Harry Hayden <h_man1992@hotmail.com>
2023-02-07 21:02:49 +0000

    add all avatars for followers section

---
+++ src/profiles/Profile.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from "../styles/Profile.module.css";
+import Avatar from '../components/Avatar';
+import { useCurrentUser } from '../contexts/CurrentUserContext';
+
+
+const Profile = (props) => {
+    const {
+        profile,
+        mobile,
+        imageSize=55
+    } = props;
+    const {
+        id,
+        following_id,
+        image,
+        owner
+    } = profile;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+
+  return (
+    <div
+        className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
+    >
+        <div>
+            <Link className='align-self-center' to={`/profiles/${id}`}>
+                <Avatar src={image} height={imageSize} />
+            </Link>
+        </div>
+        <div className={`mx-2 ${styles.Breaks}`}>
+            <strong>{owner}</strong>
+        </div>
+    </div>
+  )
+}
+
+export default Profile
